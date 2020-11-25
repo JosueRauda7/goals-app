aabf88f3fa91ce07e8de22eac867cd4d364e82a7
JosueRauda7 <katerozarack@gmail.com>
2020-11-25 09:42:22 -0600

    Add section styles and UI

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.topView}>
+        <Text style={styles.principalTitle}>GOALS APP</Text>
+        <View style={styles.addView}>
+          <TextInput
+            placeholder='Ejemplo: Hacer ejercicio'
+            style={styles.textbox}
+          />
+          <Button color='#00A8E8' title='ADD' />
+        </View>
+      </View>
+      <View style={styles.bottomView}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+  },
+  topView: {
+    backgroundColor: "#71B340",
+    paddingTop: 50,
+    padding: 40,
+  },
+  bottomView: {
+    backgroundColor: "#FFFBFF",
+  },
+  principalTitle: {
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#FAFAFF",
+  },
+  addView: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  textbox: {
+    borderBottomColor: "#FAFAFF",
+    borderBottomWidth: 1,
+    fontSize: 15,
+    padding: 5,
+    color: "#FAFAFF",
+    width: "80%",
   },
 });
